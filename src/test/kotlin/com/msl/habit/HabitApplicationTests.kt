bcd62f16f3dc1b618af8c4f405d8fc7d07f4ef61
Marcelo <marcelo.leite@mercadolivre.com>
2023-09-30 23:53:41 -0300

    [feat] start config

---
+++ src/test/kotlin/com/msl/habit/HabitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.msl.habit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HabitApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
